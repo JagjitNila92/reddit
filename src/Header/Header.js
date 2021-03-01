ef5d0eac097c9ae4c4a3f1052619e00cb9cb9e25
JagjitNila92 <jagjitnila@gmail.com>
2021-03-01 10:55:51 +0000

    Header Update with css and searchBar

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import './Header.css';
 import reddit from './reddit.png';
+import { useState } from 'react';
 
-function Header(){
+
+function Header(props){
+    const [searchItem, setSearchItem] = useState('');
+
+    const handleKeyPress = (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            props.onSearch(searchItem);
+            setSearchItem('');
+            props.history.push(`/results/`);
+        };
+    }
     
+    const handleTextChange = (e) => {
+        setSearchItem(e.target.value);
+    }
+
     return(
-        <div className="logo">
+        <div className="header">
             <img src={reddit} alt="reddit-icon"/>
-            <h1>React Client</h1>
+            <h1>Reddit <span>Micro</span></h1>
+            <div className="searchbar">
+                <label>
+                    <input type="text" id="search" placeholder="Search..." onChange={handleTextChange} value={searchItem} onKeyPress={handleKeyPress} />
+                </label>
+            </div>
         </div>
         );
 };
