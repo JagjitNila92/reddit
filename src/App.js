1753ba97f48f395b4ea0d1b0ec526d65ee74cf97
JagjitNila92 <jagjitnila@gmail.com>
2021-03-01 14:05:45 +0000

    Sidebar added to page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from './Header/Header';
+import Sidebar from './Sidebar/Sidebar';
 
 function App(){
   return(
-    <Header />
+    <div>
+          <Header />
+          <Sidebar />
+    </div>
   );
 }
 
